6359c524bd811b0d598e466a553fa173d479dbf6
varsha <varsha@evonix.co>
2016-10-01 17:41:07 +0530

    Initial Commit

---
+++ api/controllers/AController.js
@@ -0,0 +1,21 @@
+/**
+ * AController
+ *
+ * @description :: Server-side logic for managing as
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `AController.addloan()`
+   */
+  addloan: function (req, res) {
+    return res.json({
+      todo: 'addloan() is not implemented yet!'
+    });
+  }
+};
+
---
+++ api/models/Student_changed_val.js
@@ -0,0 +1,16 @@
+/**
+ * About_admin.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+    attributes: {
+        name: {type: 'string'},
+        username: {type: 'string'},
+        email: {type: 'email'},
+        about_us: {type: 'string'},
+        video_link: {type: 'string'},
+    }
+};
