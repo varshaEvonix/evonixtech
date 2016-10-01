4b3b98a1e4b8a1ad04c9dca178f6290e8d1f3c4f
varsha <varsha@evonix.co>
2016-10-01 20:21:29 +0530

    Commited

--- config/http.js
+++ config/http.js
@@ -47,26 +47,26 @@ middleware: {
 //   '404',
 //   '500'
 // ],
-passportInit    : require('passport').initialize(),
-        passportSession : require('passport').session(),
-        order: [
-                'startRequestTimer',
-                'cookieParser',
-                'session',
-                'passportInit',
-                'passportSession',
-                'myRequestLogger',
-                'bodyParser',
-                'handleBodyParserError',
-                'compress',
-                'methodOverride',
-                'poweredBy',
-                'router',
-                'www',
-                'favicon',
-                '404',
-                '500'
-        ],
+//passportInit    : require('passport').initialize(),
+//        passportSession : require('passport').session(),
+//        order: [
+//                'startRequestTimer',
+//                'cookieParser',
+//                'session',
+//                'passportInit',
+//                'passportSession',
+//                'myRequestLogger',
+//                'bodyParser',
+//                'handleBodyParserError',
+//                'compress',
+//                'methodOverride',
+//                'poweredBy',
+//                'router',
+//                'www',
+//                'favicon',
+//                '404',
+//                '500'
+//        ],
         /****************************************************************************
          *                                                                           *
          * Example custom middleware; logs each request to the console.              *
