82aba827fc393e1597ab2b1f9b87b4d9b1f0c8c2
varsha <varsha@evonix.co>
2016-10-03 15:39:37 +0530

    Commited

--- api/policies/isAuthenticated.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (req, res, ok) {
- 
-  // User is allowed, proceed to controller
-  var is_auth = req.isAuthenticated()
-  if (is_auth) return next();
-  // User is not allowed
-  else return res.redirect("/login");
-};
