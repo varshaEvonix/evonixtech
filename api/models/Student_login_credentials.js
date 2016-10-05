95e93790d52f3041f9a2e595f1697ac88372ae26
varsha <varsha@evonix.co>
2016-10-05 12:11:47 +0530

    Commit

--- api/models/Student_login_credentials.js
+++ api/models/Student_login_credentials.js
@@ -4,8 +4,8 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
-var bcrypt = require('bcrypt');
-var uuid = require('node-uuid');
+//var bcrypt = require('bcrypt');
+//var uuid = require('node-uuid');
 
 module.exports = {
     schema: true,
